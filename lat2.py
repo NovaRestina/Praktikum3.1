3c78fefd78f57f95bae5be1b16695dfaf037d506
NOVA RESTINA MAHARANI <novarestina03@gmail.com>
2022-10-29 17:21:08 +0700

    menambahkan lat2.py

---
+++ lat2.py
@@ -0,0 +1,26 @@
+a=input("masukan nilai a:")
+b=input("masukan nilai b:")
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penggabungan {1}&{0}=%s".format(a,b) %(a+b))
+
+#konversi nilai variable
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%s".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%s".format(a,b) %(a/b))
+
+
+
+print("=============================================================")
+
+print('{0:>16}'.format('*'))
+print('{0:>17}'.format('***'))
+print('{0:>18}'.format('*****'))
+print('{0:>19}'.format('*******'))
+print('{0:>20}'.format('*********'))
+print('{0:>19}'.format('*******'))
+print('{0:>18}'.format('*****'))
+print('{0:>17}'.format('***'))
+print('{0:>16}'.format('*'))
+
