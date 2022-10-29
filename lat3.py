ea17a19684281f698aba9d55745aedfc543b49a8
NOVA RESTINA MAHARANI <novarestina03@gmail.com>
2022-10-29 17:22:13 +0700

    menambahkan lat3.py

---
+++ lat3.py
@@ -0,0 +1,18 @@
+p = input("masukan nilai panjang:")
+l = input("masukan nilai lebar:")
+t = input("masukan nilai tinggi:")
+
+L = int(p) * int(l)
+K = int(p) * int(l) * int(t)
+print("Luas Persegi Panjang:", L)
+print("Keliling Persegi Panjang:", K)
+
+
+print('====================================================')
+
+r = input("Masukan jari-jari lingkaran:")
+pi = 3.14
+l = pi * int(r) * int(r)
+k = 2 * pi * int(r)
+print("Luas Lingkaran;", l)
+print("Keliling Lingkaran:", k)
